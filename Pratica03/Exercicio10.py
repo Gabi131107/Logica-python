4f782e26b05e3cb7cdb3b33d38248c4aa8844a22
Gabi131107 <gabiplapresa@gmail.com>
2026-09-02 17:43:54 -0300

    Add Exercicio10.py for user input and display

---
+++ Pratica03/Exercicio10.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+curso = input("Digite seu curso: ")
+
+print("===== FICHA =====")
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Curso: {curso}")
