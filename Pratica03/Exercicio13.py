8d2a4ac580be34fceefc755bf49963ad835a1cb3
Gabi131107 <gabiplapresa@gmail.com>
2026-09-02 17:46:18 -0300

    Add student grade calculation script

---
+++ Pratica03/Exercicio13.py
@@ -0,0 +1,11 @@
+nome = input("Nome do aluno: ")
+nota1 = float(input("Nota 1: "))
+nota2 = float(input("Nota 2: "))
+
+media = (nota1 + nota2) / 2
+
+print("===== RESULTADO =====")
+print(f"Nome: {nome}")
+print(f"Nota 1: {nota1}")
+print(f"Nota 2: {nota2}")
+print(f"Média: {media}")
